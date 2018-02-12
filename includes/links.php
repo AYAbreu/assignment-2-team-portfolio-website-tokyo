59622364c9aa77aa8becb4b25e1f8cb7cff0751e
Valerie <xwang145@u.rochester.edu>
2018-02-12 00:00:21 -0500

    changed font

--- includes/links.php
+++ includes/links.php
@@ -2,5 +2,6 @@
 <link rel="stylesheet" href="./bootstrap/css/custom.css">
 <link rel="stylesheet" type="text/css" href="./bootstrap/css/navigation.css">
 <link rel="stylesheet" type="text/css" href="./bootstrap/css/responsiveslides.css">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
     
   
