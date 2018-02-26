691162874708bace9e6f475a0c5eb4b6f2e20b97
Isaiah Pule <35773907+isaiahnkpule@users.noreply.github.com>
2018-02-26 04:24:57 -0500

    Add files via upload

---
+++ assignment3/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+  <span class="text-muted">Page created by Team Tokyo</span>
+</footer>
---
+++ assignment3/includes/nav.php
@@ -0,0 +1,15 @@
+
+<!-- Navigation bar -->
+
+<nav class="menu">
+    <div>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="Conley.html">Conley</a></li>
+        <li><a href="Fiona.html">Fiona</a></li>
+        <li><a href="Xiao.html">Xiao</a></li>
+      </ul>
+    </div>
+</nav>
+
+<!-- End of Navigation bar -->
